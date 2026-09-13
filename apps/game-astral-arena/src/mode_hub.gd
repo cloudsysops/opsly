a61269cf3d9ab37accf9798b755abf4d7699f244
Mistic <cboteros1@gmail.com>
2026-09-12 20:55:02 -0400

    feat(astral): add legal retro Games Lab and Emulator Lab

--- apps/game-astral-arena/src/mode_hub.gd
+++ apps/game-astral-arena/src/mode_hub.gd
@@ -13,7 +13,22 @@ func _ready() -> void:
     $Center/VBox/HybridBattle.pressed.connect(func():
         get_tree().change_scene_to_file("res://scenes/hybrid_battle_lab.tscn")
     )
+    $Center/VBox/GamesLab.pressed.connect(_open_games_lab)
 
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed("return_to_hub"):
         get_viewport().set_input_as_handled()
+
+
+func _open_games_lab() -> void:
+    if OS.has_feature("web"):
+        JavaScriptBridge.eval("""
+            (() => {
+              const path = window.location.pathname.endsWith('/')
+                ? window.location.pathname
+                : window.location.pathname.replace(/[^/]*$/, '');
+              window.location.href = path + 'games/';
+            })();
+        """)
+    else:
+        OS.shell_open("https://astral-arena.op-sly.com/games/")
